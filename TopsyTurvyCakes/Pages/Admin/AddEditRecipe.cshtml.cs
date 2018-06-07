8028747d607cf7bb70f007d2e178a9633df21ea3
bdbblowe <bdbblowe@gmail.com>
2018-06-07 03:22:31 -0500

    secured page from unauthorized access

--- TopsyTurvyCakes/Pages/Admin/AddEditRecipe.cshtml.cs
+++ TopsyTurvyCakes/Pages/Admin/AddEditRecipe.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -9,6 +10,7 @@ using TopsyTurvyCakes.Models;
 
 namespace TopsyTurvyCakes.Pages.Admin
 {
+    [Authorize]
     public class AddEditRecipeModel : PageModel
     {
         private readonly IRecipesService recipeService;
